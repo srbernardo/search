f2dc28e333b6116dd4dd11800eaaeeb0f5e22d65
Bernardo Brotelli <b.brotelle@gmail.com>
2025-08-11 22:09:01 -0300

    Create search migration

---
+++ db/migrate/20250812003010_create_searches.rb
@@ -0,0 +1,10 @@
+class CreateSearches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :searches do |t|
+      t.string :content
+      t.string :ip
+
+      t.timestamps
+    end
+  end
+end
